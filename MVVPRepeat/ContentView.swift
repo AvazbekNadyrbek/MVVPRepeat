f0932ccaf14a1e5ff9071cd8faf7955780883caa
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-07-15 18:46:22 +0200

    Initial Commit

---
+++ MVVPRepeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVPRepeat
+//
+//  Created by Авазбек Надырбек уулу on 15.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVPRepeat/MVVPRepeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVPRepeatApp.swift
+//  MVVPRepeat
+//
+//  Created by Авазбек Надырбек уулу on 15.07.25.
+//
+
+import SwiftUI
+
+@main
+struct MVVPRepeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
